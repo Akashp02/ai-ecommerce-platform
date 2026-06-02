cacf3d40b70469a2e85296577c49e5061641f0aa
Akash Pattanayak <pattanayakakash0204@gmail.com>
2026-06-02 10:28:27 +0530

    fix settings configuration for ci pipeline

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,9 +2,9 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_name: str
-    app_version: str
-    environment: str
+    app_name: str = "AI E-Commerce Analytics Platform"
+    app_version: str  = "1.0.0"
+    environment: str = "development"
 
     class Config:
         env_file = ".env"
